b8c7c3d228ea7ac4e67fc8f6b0cadb844dde6d3d
mankaran <gigiwahla@gmail.com>
2018-09-27 00:47:32 +0530

    Submitable

--- src/cnf_to_output.cpp
+++ src/cnf_to_output.cpp
@@ -1,57 +1,59 @@
-#include <iostream>
-#include "string.h"
-#include "input.h"
+#include<iostream>
+#include<string>
+#include<fstream>
 using namespace std;
 
-void cnf_to_output(vector<vector<int> >& subgraphs, vector<vector<int> >& graph, vector<int>& satout, long long int vertices, long long int edges, long long int num_sub);
-
-int main(int argc, char **argv){ 
-    string inputfile1name(argv[1]);	
-    string inputfile2name(argv[1]);	
+int main(int argc, char **argv){
+    if(argc!=2){
+        exit(0);
+    }
+    string inputfilename1(argv[1]);
+    string inputfilename2(argv[1]);
     string outputfilename(argv[1]);
-    inputfile1name += ".graph";
-    inputfile2name += ".satoutput";
+    inputfilename1 += ".graph";
+    inputfilename2 += ".satoutput";
     outputfilename += ".subgraphs";
-    vector<vector<int> > graph;
-    long long int vertices, edges, num_sub;
-    get_input(vertices, edges, num_sub, graph, inputfile1name);
-    vector<int> satout;
-    bool sat;
-    get_input_sat(satout, inputfile2name, sat);
-    vector<vector<int> > subgraphs;
-    if(sat){
-	    cnf_to_output(subgraphs, graph, satout, vertices, edges, num_sub);
+    ifstream infile1(inputfilename1.c_str());
+    ifstream infile2(inputfilename2.c_str());
+    ofstream outfile(outputfilename.c_str());
+    if(!(infile1.is_open())){
+        cout << "There is no such input file" << endl;
+        exit(1);
     }
-	final_output(subgraphs, sat, outputfilename);
-
-    for(int i = 0; i<graph.size(); i++){
-    	cout << i << ": ";
-    	for(int j = 0; j< graph[i].size(); j++){
-    		cout << graph[i][j] << " ";
-    	}
-    	cout << endl;
-    }cout << endl;
-
-    for(int i = 0; i<satout.size(); i++){
-    	cout << satout[i] << " ";
-    }cout << endl;
-    
+    if(!(infile2.is_open())){
+        cout << "There is no such input file" << endl;
+        exit(2);
+    }
+    if(!(outfile).is_open()){
+        cout << "There is no such output file" << endl;
+        exit(3);
+    }
+    string ans;
+    infile2 >> ans;
+    ans = ans.substr(0,2);
+    if(ans.compare("UN")==0){
+        outfile << 0 << endl;
+        outfile.close();
+    }
+    else{
+        int nv,ne,nsg;
+        infile1 >> nv >> ne >> nsg;
+        infile1.close();
+        for(int i=1;i<=nsg;i++){
+            outfile << "#" + to_string(i) + " ";
+            int a,count = 0;
+            string s = "";
+            for(int j=1;j<=nv;j++){
+                infile2 >> a;
+                if(a>0){
+                    count++;
+                    s += to_string(j) + " ";
+                }
+            }
+            outfile << count << endl;
+            outfile << s.substr(0,s.length() - 1) << endl;
+        }
+        outfile.close();    
+    }
     return 0;
-}
-
-void cnf_to_output(vector<vector<int> >& subgraphs, vector<vector<int> >& graph, vector<int>& satout, long long int vertices, long long int edges, long long int num_sub){
-    for(int i = 0; i<3; i++){
-        vector<int> v;
-        subgraphs.push_back(v);
-    }
-
-    subgraphs[0].push_back(1);
-    subgraphs[0].push_back(2);
-    subgraphs[0].push_back(3);
-    subgraphs[1].push_back(1);
-    subgraphs[1].push_back(4);
-    subgraphs[1].push_back(5);
-    subgraphs[1].push_back(2);
-    subgraphs[2].push_back(3);
-    subgraphs[2].push_back(4);
 }
--- src/graph_to_cnf.cpp
+++ src/graph_to_cnf.cpp
@@ -1,48 +1,145 @@
-#include <iostream>
-#include "string.h"
-#include "input.h"
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<fstream>
+#include<map>
+#include<cstdlib> 
+#include<vector>
 using namespace std;
 
-void graph_to_cnf(vector<vector<int> >& graph, vector<vector<int> >& clauses, long long int& num_variables, long long int vertices, long long int edges, long long int num_sub);
-
-int main(int argc, char **argv){ 
+int main(int argc, char **argv){
+    if(argc!=2){
+        exit(0);
+    }
     string inputfilename(argv[1]);
     string outputfilename(argv[1]);
     inputfilename += ".graph";
     outputfilename += ".satinput";
-    vector<vector<int> > graph;
-    long long int vertices, edges, num_sub;
-    get_input(vertices, edges, num_sub, graph, inputfilename);
-    vector<vector<int> > clauses;
-    long long int num_variables = 0;
-    graph_to_cnf(graph, clauses, num_variables, vertices, edges, num_sub);
-    output_to_sat(clauses, num_variables, outputfilename);
-
-    for(int i = 0; i<graph.size(); i++){
-    	cout << i << ": ";
-    	for(int j = 0; j< graph[i].size(); j++){
-    		cout << graph[i][j] << " ";
-    	}
-    	cout << endl;
+    ifstream infile(inputfilename.c_str());
+    ofstream outfile(outputfilename.c_str());
+    if(!(infile.is_open())){
+        cout << "There is no such input file" << endl;
+        exit(1);
     }
-
+    int nv,ne,nsg;
+    infile >> nv >> ne >> nsg;
+    bool present[nsg+1][nv+1];
+    vector<pair<int, int> > edges;
+    int a,b;
+    for(int i=0;i<ne;i++){
+        infile >> a >> b;
+        edges.push_back(make_pair(a,b));
+        edges.push_back(make_pair(b,a));
+    }
+    infile.close();
+    int numVar=0;
+    vector<string> clauses;
+    map<pair<int, int>, int> mpresent,medges,edgepresent;
+    int mapping[nsg+1][nsg+1][nv+1],mapping2[nsg+1][nsg+1][nv+1];
+    string ab;
+    for(int i=1;i<=nsg;i++){
+        for(int j=1;j<=nv;j++){
+            numVar++;
+            mpresent[make_pair(i,j)] = numVar;
+        }
+    }
+    cout << nsg << " " << nv << " " << numVar << endl;
+    for(int i=1;i<=nsg;i++){
+        for(int j=i+1;j<=nsg;j++){
+            for(int k=1;k<=nv;k++){
+                numVar++;
+                mapping[i][j][k] = numVar;
+                ab = "-" + to_string(numVar) + " -" + to_string(mpresent[make_pair(i,k)])  + "  0";
+                clauses.push_back(ab);
+                ab = "-" + to_string(numVar) + " " + to_string(mpresent[make_pair(j,k)]) + "  0";
+                clauses.push_back(ab);
+                ab = to_string(numVar) + " " + to_string(mpresent[make_pair(i,k)]) + " -" + to_string(mpresent[make_pair(j,k)]) + "  0";
+                clauses.push_back(ab);
+                numVar++;
+                mapping2[i][j][k] = numVar;
+                ab = "-" + to_string(numVar) + " " + to_string(mpresent[make_pair(i,k)])  + "  0";
+                clauses.push_back(ab);
+                ab = "-" + to_string(numVar) + " -" + to_string(mpresent[make_pair(j,k)]) + "  0";
+                clauses.push_back(ab);
+                ab = to_string(numVar) + " -" + to_string(mpresent[make_pair(i,k)]) + " " + to_string(mpresent[make_pair(j,k)]) + "  0";
+                clauses.push_back(ab);
+            }
+        }
+    }
+    for(int i=0;i<edges.size();i++){
+        numVar++;
+        int a = edges.at(i).first;
+        int b = edges.at(i).second;
+        medges[make_pair(a,b)] = numVar;
+        ab = to_string(numVar) + "  0";
+        clauses.push_back(ab);
+    }
+    for(int i=0;i<edges.size();i++){
+        ab = "";
+        int a = edges.at(i).first;
+        int b = edges.at(i).second;
+        for(int j=1;j<=nsg;j++){
+            numVar++;
+            edgepresent[make_pair(i,j)] = numVar;
+            ab += (to_string(numVar) + " ");
+            string t1 = "-" + to_string(numVar) + " " + to_string(mpresent[make_pair(j,a)]) + "  0";
+            string t2 = "-" + to_string(numVar) + " " + to_string(mpresent[make_pair(j,b)]) + "  0";
+            string t3 = "-" + to_string(mpresent[make_pair(j,a)]) + " -" + to_string(mpresent[make_pair(j,b)]) + " " + to_string(numVar) + "  0";
+            clauses.push_back(t1);
+            clauses.push_back(t2);
+            clauses.push_back(t3);
+        }
+        ab += " 0";
+        clauses.push_back(ab);
+    }
+    for(int i=1;i<=nsg;i++){
+        for(int j=1;j<=nv;j++){
+            for(int k=j+1;k<=nv;k++){
+                bool exist = false;
+                for(int l=0;l<edges.size();l++){
+                    if(((edges.at(l).first==j && edges.at(l).second==k)||(edges.at(l).first==k && edges.at(l).second==j))){
+                        exist = true;
+                        break;
+                    }
+                }
+                if(exist==false){
+                    ab = "-" + to_string(mpresent[make_pair(i,j)]) + " -" + to_string(mpresent[make_pair(i,k)]) + "  0";
+                    clauses.push_back(ab);
+                }
+            }
+        }
+    }
+    for(int i=1;i<=nsg;i++){
+        ab = "";
+        for(int j=0;j<edges.size();j++){
+            ab += (to_string(edgepresent[make_pair(j,i)]) + " ");
+        }
+        ab += " 0";
+        clauses.push_back(ab);
+    }
+    string bc;
+    for(int i=1;i<=nsg;i++){
+        for(int j=i+1;j<=nsg;j++){
+            ab = "";
+            bc = "";
+            for(int k=1;k<=nv;k++){
+                ab +=  to_string(mapping[i][j][k]) + " ";
+                bc +=  to_string(mapping2[i][j][k]) + " ";
+            }
+            ab += " 0";
+            bc += " 0";
+            clauses.push_back(ab);
+            clauses.push_back(bc);
+        }
+    }
+    if(!(outfile).is_open()){
+        cout << "There is no such output file" << endl;
+        exit(2);
+    }
+    outfile << ("p cnf " + to_string(numVar) + " " + to_string(clauses.size())) << endl;
+    for(int i=0;i<clauses.size();i++){
+        outfile << clauses.at(i) << endl;
+    }
+    outfile.close();
     return 0;
-}
-
-void graph_to_cnf(vector<vector<int> >& graph, vector<vector<int> >& clauses, long long int& num_variables, long long int vertices, long long int edges, long long int num_sub){
-    for(int i = 0; i<3; i++){
-        vector<int> v;
-        clauses.push_back(v);
-    }
-    num_variables = 5;
-
-    clauses[0].push_back(1);
-    clauses[0].push_back(-5);
-    clauses[0].push_back(4);
-    clauses[1].push_back(-1);
-    clauses[1].push_back(5);
-    clauses[1].push_back(3);
-    clauses[1].push_back(4);
-    clauses[2].push_back(-3);
-    clauses[2].push_back(-4);
 }
