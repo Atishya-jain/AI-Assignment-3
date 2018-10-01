96b667111718a4cbeff0c4fc90bc4bc97024c92e
mankaran <gigiwahla@gmail.com>
2018-10-01 12:32:15 +0530

    Probably the final commit

--- src/compile.sh
+++ src/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-g++ --std=c++11 graph_to_cnf.cpp -o main1
-g++ --std=c++11 cnf_to_output.cpp -o main2
+g++ -O3 --std=c++11 graph_to_cnf.cpp -o main1
+g++ -O3 --std=c++11 cnf_to_output.cpp -o main2
 exit 0
